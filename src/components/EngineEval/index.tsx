00ff0cc1f64cf4cfb459b741dd496b1be1105566
Xander <xandernaumenko@gmail.com>
2021-08-25 21:38:16 -0700

    Cleaned up old asynchronous cod and updated types for node-uci

--- src/components/EngineEval/index.tsx
+++ src/components/EngineEval/index.tsx
@@ -1,29 +1,42 @@
 import { FieldTimeOutlined, UploadOutlined } from '@ant-design/icons';
-import { Button, Card, Col, DatePicker, Drawer, Form, Input, Row, Select, Upload } from 'antd';
+import {
+  Button,
+  Card,
+  Col,
+  DatePicker,
+  Drawer,
+  Form,
+  Input,
+  Row,
+  Select,
+  Upload,
+} from 'antd';
 const { Option } = Select;
 import Title from 'antd/lib/typography/Title';
 import { UploadChangeParam } from 'antd/lib/upload';
+import { EngineOptions } from 'node-uci';
 // import { Engine } from 'node-uci';
 import React, { useState } from 'react';
 
 // import { dialog, UploadFile } from 'electron';
-const electron = window.require('electron')
-const remote = electron.remote
-const {dialog} = remote
-const {ipcRenderer } = require('electron')
-
+const electron = window.require('electron');
+const remote = electron.remote;
+const { dialog } = remote;
+const { ipcRenderer } = require('electron');
 
 const EngineEval = () => {
-  // const [engine, setEngine] = useState<Engine | null>(null);
   const [newEngineVisible, setNewEngineVisible] = useState(false);
-  const [engineId, setEngineId] = useState({name: undefined, author: undefined})
-  const [engineFilePath, setEngineFilePath] = useState('')
-  const [engineOptions, setEngineOptions] = useState()
+  const [engineId, setEngineId] = useState({
+    name: undefined,
+    author: undefined,
+  });
+  const [engineFilePath, setEngineFilePath] = useState('');
+  const [engineOptions, setEngineOptions] = useState<EngineOptions>(new Map());
 
+  // Engine selection dropdown
   const onEngineSelect = (option: string) => {
     if (option == 'new') {
       setNewEngineVisible(true);
-      // setEngine(null)
     }
   };
 
@@ -31,40 +44,28 @@ const EngineEval = () => {
     setNewEngineVisible(false);
   };
 
-  // console.log('init')
-  // const e = new Engine('C:\\tools\\Chess Engines\\komodo-12.1.1-64bit-bmi2.exe')
-  // e.init().then(eng => {console.log("outer engine initialized")})
-  // new Promise(resolve => setTimeout(resolve, 500)).then(()=>{console.log(e.id)})
-
-
+  // Engine initialization event
   ipcRenderer.on('engine-init', (event, id, filePath, options) => {
-    setEngineId(id)
-    setEngineFilePath(filePath)
-    setEngineOptions(options)
-    console.log('id received')
-    console.log(id)
-  })
+    setEngineId(id);
+    setEngineFilePath(filePath);
+    setEngineOptions(options);
+    console.log('id received');
+    console.log(id);
+  });
 
+  // New engine upload triggers ipc communication to init engine
   const newEngineUpload = () => {
-    console.log('clicked')
-    // dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }).then(result => {
-    //   // let eng = new Engine(result.filePaths[0])
-    //   let eng = new Engine('C:\\tools\\Chess Engines\\komodo-12.1.1-64bit-bmi2.exe')
-    //   console.log('dialog returned')
-    //   // console.log(eng)
-    //   // eng.chain().init().exec()
-    //   return eng.init()
-    //   // await new Promise(resolve => setTimeout(resolve, 3000))
-    //   // console.log('engine initialized')
-    //   // setEngine(eng)
-    // }).then(eng => {
-    //   console.log(eng)
-    // })
-    // new Engine('C:\\tools\\Chess Engines\\komodo-12.1.1-64bit-bmi2.exe').init().then(eng => {console.log("inner engine initialized")})
-    // new Promise(resolve => setTimeout(resolve, 3000)).then(()=>{console.log("timier finished")})
+    console.log('clicked');
+    ipcRenderer.send('engine-init');
+  };
 
-    ipcRenderer.send('engine-init')
-  }
+  const optionsEntries: JSX.Element[] = [];
+
+  engineOptions.forEach((value, key, map) => {
+    optionsEntries.push(<Form.Item>
+
+    </Form.Item>)
+  });
 
   return (
     <>
@@ -93,92 +94,90 @@ const EngineEval = () => {
         width={720}
       >
         <Form layout="vertical" hideRequiredMark>
-            <Row gutter={16}>
+          <Row gutter={16}>
+            <Form.Item name="upload" label="Engine Executable Upload">
+              <Button icon={<UploadOutlined />} onClick={newEngineUpload}>
+                Click to Upload
+              </Button>
+            </Form.Item>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <p>Engine Name:</p>
+              <Input disabled value={engineId.name} />
+            </Col>
+            <Col span={12}>
+              <p>Engine Author(s):</p>
+              <Input disabled value={engineId.author} />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <p>Engine Path: </p>
+              <Input disabled value={engineFilePath} />
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
               <Form.Item
-                name="upload"
-                label="Engine Executable Upload"
+                name="type"
+                label="Type"
+                rules={[{ required: true, message: 'Please choose the type' }]}
               >
-                <Button icon={<UploadOutlined/>} onClick={newEngineUpload}>
-                  Click to Upload
-                </Button>
+                <Select placeholder="Please choose the type">
+                  <Option value="private">Private</Option>
+                  <Option value="public">Public</Option>
+                </Select>
               </Form.Item>
-            </Row>
-            <Row gutter={16}>
-              <Col span={12}>
-                <p>
-                  Engine Name: 
-                </p>
-                <Input disabled value={engineId.name}/>
-              </Col>
-              <Col span={12}>
-                <p>
-                  Engine Author(s): 
-                </p>
-                <Input disabled value={engineId.author}/>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={24}>
-                <p>Engine Path: </p>
-                <Input disabled value={engineFilePath}/>
-              </Col>
-            </Row>
-            <Row gutter={16}>
-              <Col span={12}>
-                <Form.Item
-                  name="type"
-                  label="Type"
-                  rules={[{ required: true, message: 'Please choose the type' }]}
-                >
-                  <Select placeholder="Please choose the type">
-                    <Option value="private">Private</Option>
-                    <Option value="public">Public</Option>
-                  </Select>
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row gutter={16}>
-              <Col span={12}>
-                <Form.Item
-                  name="approver"
-                  label="Approver"
-                  rules={[{ required: true, message: 'Please choose the approver' }]}
-                >
-                  <Select placeholder="Please choose the approver">
-                    <Option value="jack">Jack Ma</Option>
-                    <Option value="tom">Tom Liu</Option>
-                  </Select>
-                </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Form.Item
-                  name="dateTime"
-                  label="DateTime"
-                  rules={[{ required: true, message: 'Please choose the dateTime' }]}
-                >
-                  <DatePicker.RangePicker
-                    style={{ width: '100%' }}
-                  />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row gutter={16}>
-              <Col span={24}>
-                <Form.Item
-                  name="description"
-                  label="Description"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'please enter url description',
-                    },
-                  ]}
-                >
-                  <Input.TextArea rows={4} placeholder="please enter url description" />
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="approver"
+                label="Approver"
+                rules={[
+                  { required: true, message: 'Please choose the approver' },
+                ]}
+              >
+                <Select placeholder="Please choose the approver">
+                  <Option value="jack">Jack Ma</Option>
+                  <Option value="tom">Tom Liu</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="dateTime"
+                label="DateTime"
+                rules={[
+                  { required: true, message: 'Please choose the dateTime' },
+                ]}
+              >
+                <DatePicker.RangePicker style={{ width: '100%' }} />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={24}>
+              <Form.Item
+                name="description"
+                label="Description"
+                rules={[
+                  {
+                    required: true,
+                    message: 'please enter url description',
+                  },
+                ]}
+              >
+                <Input.TextArea
+                  rows={4}
+                  placeholder="please enter url description"
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
       </Drawer>
     </>
   );
--- types/node-uci/index.d.ts
+++ types/node-uci/index.d.ts
@@ -7,58 +7,70 @@
 /// <reference types="node" />
 
 declare module 'node-uci' {
+  import { EventEmitter } from 'events';
 
-    import { EventEmitter } from 'events';
+  export interface SearchOptions {
+    searchmoves?: ReadonlyArray<string> | undefined;
+    ponder?: boolean | undefined;
+    wtime?: number | undefined;
+    btime?: number | undefined;
+    winc?: number | undefined;
+    binc?: number | undefined;
+    movestogo?: number | undefined;
+    depth?: number | undefined;
+    nodes?: number | undefined;
+    mate?: number | undefined;
+    movetime?: number | undefined;
+  }
 
-    export interface SearchOptions {
-        searchmoves?: ReadonlyArray<string> | undefined;
-        ponder?: boolean | undefined;
-        wtime?: number | undefined;
-        btime?: number | undefined;
-        winc?: number | undefined;
-        binc?: number | undefined;
-        movestogo?: number | undefined;
-        depth?: number | undefined;
-        nodes?: number | undefined;
-        mate?: number | undefined;
-        movetime?: number | undefined;
+  export interface SearchResult {
+    bestmove: string;
+    info: ReadonlyArray<string>;
+  }
+
+  export type EngineOptions = Map<
+    string,
+    {
+      type: string;
+      default: boolean | number | string;
+      min?: number;
+      max?: number;
+      options?: [string];
     }
+  >;
 
-    export interface SearchResult {
-        bestmove: string;
-        info: ReadonlyArray<string>;
-    }
+  export class Engine {
+    constructor(enginePath: string);
+    getBufferUntil(
+      fn: (str: string) => boolean
+    ): Promise<ReadonlyArray<string>>;
+    write(cmd: string): void;
+    chain(): EngineChain;
+    init(): Promise<Engine>;
+    quit(): Promise<Engine>;
+    isready(): Promise<Engine>;
+    sendCmd(cmd: string): Promise<Engine>;
+    setoption(name: string, value?: string): Promise<Engine>;
+    ucinewgame(): Promise<Engine>;
+    ponderhit(): Promise<Engine>;
+    position(fen: string, moves?: ReadonlyArray<string>): Promise<Engine>;
+    go(sp: SearchOptions): Promise<SearchResult>;
+    goInfinite(sp: SearchOptions): EventEmitter;
+    stop(): Promise<SearchResult>;
+    id: { name: string; author: string };
+    filePath: string;
+    options: EngineOptions;
+  }
 
-    export class Engine {
-        constructor(enginePath: string);
-        getBufferUntil(fn: (str: string) => boolean): Promise<ReadonlyArray<string>>;
-        write(cmd: string): void;
-        chain(): EngineChain;
-        init(): Promise<Engine>;
-        quit(): Promise<Engine>;
-        isready(): Promise<Engine>;
-        sendCmd(cmd: string): Promise<Engine>;
-        setoption(name: string, value?: string): Promise<Engine>;
-        ucinewgame(): Promise<Engine>;
-        ponderhit(): Promise<Engine>;
-        position(fen: string, moves?: ReadonlyArray<string>): Promise<Engine>;
-        go(sp: SearchOptions): Promise<SearchResult>;
-        goInfinite(sp: SearchOptions): EventEmitter;
-        stop(): Promise<SearchResult>;
-        id: {name: string, author: string};
-        filePath: string
-        options: Map<string, {type: string, default: boolean | number | string, min?: number, max?: number, options?: [string]}>
-    }
-
-    export class EngineChain {
-        constructor(engine: Engine);
-        init(): EngineChain;
-        setoption(name: string, value?: string): EngineChain;
-        isready(): EngineChain;
-        ucinewgame(): EngineChain;
-        quit(): EngineChain;
-        position(fen: string, moves?: ReadonlyArray<string>): EngineChain;
-        go(sp: SearchOptions): Promise<SearchResult>;
-        exec(): unknown;
-    }
+  export class EngineChain {
+    constructor(engine: Engine);
+    init(): EngineChain;
+    setoption(name: string, value?: string): EngineChain;
+    isready(): EngineChain;
+    ucinewgame(): EngineChain;
+    quit(): EngineChain;
+    position(fen: string, moves?: ReadonlyArray<string>): EngineChain;
+    go(sp: SearchOptions): Promise<SearchResult>;
+    exec(): unknown;
+  }
 }
